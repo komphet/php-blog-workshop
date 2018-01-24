35a545ff842aff94c36f45657374a20ce9092eec
komphet <komphet@kmi.tl>
2018-01-24 23:02:11 +0700

    update'

--- article_create_exec.php
+++ article_create_exec.php
@@ -11,9 +11,9 @@ if(isset($title) && isset($content)) {
 	$str = "INSERT INTO articles (title, content) VALUES('$title','$content')";
 	 $query = mysqli_query($conn, $str);
 	 if(!$query){
-	 	echo "<div style='color:red'>ไม่สามารถบันทึกข้อมูลได้!</div>";
+	 	echo "<div style='color:red;'>ไม่สามารถบันทึกข้อมูลได้!</div>";
 	 } else {
-         echo "<div style='color:red'>ไม่สามารถบันทึกข้อมูลได้!</div>";
+         echo "<div style='color:green;'>บันทึกข้อมูลเรียบร้อยแล้ว!</div>";
      }
 }
 
