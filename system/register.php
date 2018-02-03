5a403b8f9240573f5eb5b582f892f273ff6be888
komphet <komphet@kmi.tl>
2018-02-03 23:41:14 +0700

    Finished Register Page

--- system/register.php
+++ system/register.php
@@ -1,3 +1,51 @@
+<?php
+$errors = [];
+$requests = [];
+
+if (isset($_POST['username'])) {
+    require "system/config.php";
+    $inputs = [
+        'username',
+        'password',
+        'password_confirmation',
+        'email',
+        'firstname',
+        'lastname'
+    ];
+    $errors = [];
+    foreach ($inputs as $input) {
+        $request = $_POST[$input];
+        if (is_null($request) || $request == '') {
+            $errors[$input] = ucfirst($input) . " is required!";
+        }
+        $requests[$input] = $request;
+    }
+    if ($requests['password'] != $requests['password_confirmation']) {
+        $errors['password_confirmation'] = "Password confirmation do not match!";
+    }
+
+    if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM users WHERE username = '" . $requests['username'] . "' OR email = '" . $requests['email'] . "';")) > 0) {
+        $errors['username'] = "Username or Email has been taken!";
+    }
+
+    if (count($errors) <= 0) {
+        $query = mysqli_query($conn, "INSERT INTO users VALUES(NULL,"
+            . "'" . $requests['username'] . "',"
+            . "'" . md5($requests['password']) . "',"
+            . "'" . $requests['email'] . "',"
+            . "'" . $requests['firstname'] . "',"
+            . "'" . $requests['lastname'] . "',"
+            . "NOW(),NOW()"
+            . ");") or die(mysqli_error($conn));
+        if ($query) {
+            echo "<meta http-equiv=\"refresh\" content=\"0;url=/?page=login&msg=". urlencode("Registration successfully. Please sign in.")."\">";
+            echo "Redirecting...";
+            exit();
+        }
+    }
+}
+?>
+
 <div class="tm-home-img-container">
     <div>
         Register
@@ -13,10 +61,17 @@
                     <legend>Personal Information</legend>
                     <div class="row">
                         <div class="col-sm-6 m-b-1">
-                            <input type="text" class="form-control" name="firstname" placeholder="Firstname">
+                            <input value="<?php echo array_key_exists("firstname", $requests) ? $requests['firstname'] : ''; ?>"
+                                   type="text" class="form-control" name="firstname" placeholder="Firstname">
+                            <?php if (array_key_exists("firstname", $errors)): ?>
+                                <div class="text-danger"><?php echo $errors['firstname']; ?></div>
+                            <?php endif ?>
                         </div>
                         <div class="col-sm-6 m-b-1">
-                            <input type="text" class="form-control" name="lastname" placeholder="Lastname">
+                            <input value="<?php echo array_key_exists("lastname", $requests) ? $requests['lastname'] : ''; ?>" type="text" class="form-control" name="lastname" placeholder="Lastname">
+                            <?php if (array_key_exists("lastname", $errors)): ?>
+                                <div class="text-danger"><?php echo $errors['lastname']; ?></div>
+                            <?php endif ?>
                         </div>
                     </div>
                 </fieldset>
@@ -24,16 +79,29 @@
                     <legend>System Information</legend>
                     <div class="row">
                         <div class="col-sm-6 m-b-1">
-                            <input type="text" class="form-control" name="username" placeholder="Username">
+                            <input value="<?php echo array_key_exists("username", $requests) ? $requests['username'] : ''; ?>" type="text" class="form-control" name="username" placeholder="Username">
+                            <?php if (array_key_exists("username", $errors)): ?>
+                                <div class="text-danger"><?php echo $errors['username']; ?></div>
+                            <?php endif ?>
                         </div>
                         <div class="col-sm-6 m-b-1">
-                            <input type="email" class="form-control" name="email" placeholder="Email">
+                            <input value="<?php echo array_key_exists("email", $requests) ? $requests['email'] : ''; ?>" type="email" class="form-control" name="email" placeholder="Email">
+                            <?php if (array_key_exists("email", $errors)): ?>
+                                <div class="text-danger"><?php echo $errors['email']; ?></div>
+                            <?php endif ?>
                         </div>
                         <div class="col-sm-6 m-b-1">
                             <input type="password" class="form-control" name="password" placeholder="Password">
+                            <?php if (array_key_exists("password", $errors)): ?>
+                                <div class="text-danger"><?php echo $errors['password']; ?></div>
+                            <?php endif ?>
                         </div>
                         <div class="col-sm-6 m-b-1">
-                            <input type="password" class="form-control" name="password_confirmation" placeholder="Retype Password">
+                            <input type="password" class="form-control" name="password_confirmation"
+                                   placeholder="Retype Password">
+                            <?php if (array_key_exists("password_confirmation", $errors)): ?>
+                                <div class="text-danger"><?php echo $errors['password_confirmation']; ?></div>
+                            <?php endif ?>
                         </div>
                     </div>
                 </fieldset>
@@ -42,3 +110,4 @@
         </form>
     </div>
 </section>
+
