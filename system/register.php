0078c63acb60f44321cd0b2120351d4219b44a61
komphet <komphet@kmi.tl>
2018-02-03 22:31:24 +0700

    Update Register Page

--- system/register.php
+++ system/register.php
@@ -7,36 +7,38 @@
 
 <section class="tm-section">
     <div class="container">
-        <div class="col-sm-8 col-md-offset-2">
-            <fieldset>
-                <legend>Personal Information</legend>
-                <div class="row">
-                    <div class="col-sm-6 m-b-1">
-                        <input type="text" class="form-control" placeholder="Firstname">
+        <form action="/?page=register" method="post">
+            <div class="col-sm-8 col-md-offset-2">
+                <fieldset>
+                    <legend>Personal Information</legend>
+                    <div class="row">
+                        <div class="col-sm-6 m-b-1">
+                            <input type="text" class="form-control" name="firstname" placeholder="Firstname">
+                        </div>
+                        <div class="col-sm-6 m-b-1">
+                            <input type="text" class="form-control" name="lastname" placeholder="Lastname">
+                        </div>
                     </div>
-                    <div class="col-sm-6 m-b-1">
-                        <input type="text" class="form-control" placeholder="Lastname">
+                </fieldset>
+                <fieldset>
+                    <legend>System Information</legend>
+                    <div class="row">
+                        <div class="col-sm-6 m-b-1">
+                            <input type="text" class="form-control" name="username" placeholder="Username">
+                        </div>
+                        <div class="col-sm-6 m-b-1">
+                            <input type="email" class="form-control" name="email" placeholder="Email">
+                        </div>
+                        <div class="col-sm-6 m-b-1">
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                        </div>
+                        <div class="col-sm-6 m-b-1">
+                            <input type="password" class="form-control" name="password_confirmation" placeholder="Retype Password">
+                        </div>
                     </div>
-                </div>
-            </fieldset>
-            <fieldset>
-                <legend>System Information</legend>
-                <div class="row">
-                    <div class="col-sm-6 m-b-1">
-                        <input type="text" class="form-control" placeholder="Username">
-                    </div>
-                    <div class="col-sm-6 m-b-1">
-                        <input type="email" class="form-control" placeholder="Email">
-                    </div>
-                    <div class="col-sm-6 m-b-1">
-                        <input type="password" class="form-control" placeholder="Password">
-                    </div>
-                    <div class="col-sm-6 m-b-1">
-                        <input type="password_confirmation" class="form-control" placeholder="Retype Password">
-                    </div>
-                </div>
-            </fieldset>
-            <input type="submit" value="REGISTER" class="btn btn-primary btn-lg" style="width: 100%;">
-        </div>
+                </fieldset>
+                <input type="submit" value="REGISTER" class="btn btn-primary btn-lg" style="width: 100%;">
+            </div>
+        </form>
     </div>
 </section>
