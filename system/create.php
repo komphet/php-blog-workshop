56783d8af318a1955ae3bc4b0b745549daa0b329
komphet <komphet@kmi.tl>
2018-02-04 01:21:18 +0700

    Finished Article creation page

--- system/create.php
+++ system/create.php
@@ -1,3 +1,35 @@
+<?php
+if (is_null($_SESSION['user'])) {
+    echo "<meta http-equiv=\"refresh\" content=\"0;url=/?page=login&msg=" . urlencode("Please Login.") . ".\">";
+    echo "Redirecting...";
+    exit();
+}
+
+$errors = [];
+$requests = [];
+
+if (isset($_POST['title'])) {
+    require "system/config.php";
+    $inputs = [
+        'title',
+        'content',
+    ];
+    foreach ($inputs as $input) {
+        $request = $_POST[$input];
+        if (is_null($request) || $request == '') {
+            $errors[$input] = ucfirst($input) . " is required!";
+        }
+        $requests[$input] = $request;
+    }
+
+    if (count($errors) <= 0) {
+        mysqli_query($conn, "INSERT INTO blogs VALUES(NULL, " . $_SESSION['user']['id'] . ",'" . $requests['title'] . "','" . $requests['content'] . "',NOW(),NOW())") or die(mysqli_error($conn));
+        echo "<meta http-equiv=\"refresh\" content=\"0;url=/?page=create&msg=" . urlencode("Create Blog successfully.") . ".\">";
+        echo "Redirecting...";
+        exit();
+    }
+}
+?>
 <div class="tm-home-img-container">
     <div>
         Create Blog
@@ -10,11 +42,21 @@
 <script src="/assets/summernote/dist/summernote.js"></script>
 <section class="tm-section">
     <div class="container">
-        <input type="text" class="form-control" placeholder="Title">
-        <form method="post">
-            <textarea id="summernote" name="editordata"></textarea>
+        <?php if (count($errors) > 0): ?>
+            <div class="alert alert-danger">
+                <ul>
+                    <?php foreach ($errors as $error): ?>
+                        <li><?php echo $error; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+        <form action="/?page=create" method="post">
+            <input type="text" name="title" class="form-control" placeholder="Title">
+            <textarea id="summernote" name="content"></textarea>
+            <input type="submit" value="Save" class="btn btn-success btn-lg" style="width: 100%;">
         </form>
-        <input type="submit" value="Save" class="btn btn-success btn-lg" style="width: 100%;">
+
     </div>
 </section>
 
