2a44a137d699ed09c312324fd5d7d2b579d6bf68
komphet <komphet@kmi.tl>
2018-02-04 01:01:41 +0700

    Finished Forgot Password Page

--- system/logout.php
+++ system/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-session_destroy ();
+session_destroy();
 echo "<meta http-equiv=\"refresh\" content=\"0;url=/?msg=". urlencode("Signed out successfully!").".\">";
 echo "Redirecting...";
 exit();
